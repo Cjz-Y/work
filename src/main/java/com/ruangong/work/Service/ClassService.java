2c977ec9bd5cc6d40d0cba263d5aeea52f74ced3
Cjz.Y <cjz.y@hotmail.com>
2018-12-06 21:49:59 +0800

    修改Class错误

--- src/main/java/com/ruangong/work/Service/ClassService.java
+++
@@ -1,8 +0,0 @@
-package com.ruangong.work.Service;
-
-import java.util.List;
-
-public interface ClassService extends GeneralService<Class, Long> {
-
-    List<Class> findAll();
-}
---
+++ src/main/java/com/ruangong/work/Service/Impl/BanServiceImpl.java
@@ -0,0 +1,33 @@
+package com.ruangong.work.Service.Impl;
+
+import com.ruangong.work.Bean.Ban;
+import com.ruangong.work.Repository.BanRepository;
+import com.ruangong.work.Repository.GeneralRepository;
+import com.ruangong.work.Service.BanService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Transactional
+@Service("banService")
+public class BanServiceImpl extends GeneralServiceImpl<Ban, Long> implements BanService {
+
+    private BanRepository banRepository;
+
+
+    @Override
+    @Autowired
+    @Qualifier("banRepository")
+    public void setGeneralRepository(GeneralRepository<Ban, Long> generalRepository) {
+        this.generalRepository = generalRepository;
+        this.banRepository = (BanRepository) generalRepository;
+    }
+
+    @Override
+    public List<Ban> findAll(){
+        return banRepository.findAll();
+    }
+}
